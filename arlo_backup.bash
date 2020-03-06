9a91844a481722bc2dd14acf3c0559cadb2a4034
Greg Kulasik <gkulasik@gmail.com>
2020-03-05 21:20:59 -0600

    Fix issues with bash syntax for ssh and rsync

--- arlo_backup.bash
+++ arlo_backup.bash
@@ -11,8 +11,8 @@ mkdir -p videos
 echo "Starting python script"
 #python3 arlo-backup.py -u $ARLO_USERNAME -p $ARLO_PASSWORD || exit 1
 
-echo "Starting transfer to ${ARLO_BACKUP_USER}@${ARLO_BACKUP_IP}${ARLO_BACKUP_PATH}/${date_formatted}..."
-ssh  ${ARLO_BACKUP_USER}@${ARLO_BACKUP_IP} "cd ${ARLO_BACKUP_PATH}; mkdir -p ${date_formatted}" || exit 1
-rsync --remove-source-files --dry-run /videos ${ARLO_BACKUP_USER}:${ARLO_BACKUP_IP}:${ARLO_BACKUP_PATH}/${date_formatted} || exit 1
+echo "Starting transfer to ${ARLO_BACKUP_USER}@${ARLO_BACKUP_IP}:${ARLO_BACKUP_PATH}/${date_formatted}..."
+ssh ${ARLO_BACKUP_USER}@${ARLO_BACKUP_IP} mkdir -p ${ARLO_BACKUP_PATH}/${date_formatted} || exit 1
+rsync --remove-source-files -av videos/ ${ARLO_BACKUP_USER}@${ARLO_BACKUP_IP}:${ARLO_BACKUP_PAT2H}/${date_formatted} || exit 1
 
 echo "Arlo backup completed"
