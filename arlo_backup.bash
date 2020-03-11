99e9835d4a7de283966acc67b0160add9b35fc17
Greg Kulasik <gkulasik@gmail.com>
2020-03-11 10:20:50 -0500

    More descriptive comments

--- arlo_backup.bash
+++ arlo_backup.bash
@@ -15,7 +15,10 @@ echo "Starting python script"
 python3 arlo-backup.py -u $ARLO_USERNAME -p $ARLO_PASSWORD || exit 1
 
 echo "Starting transfer to ${ARLO_BACKUP_USER}@${ARLO_BACKUP_IP}:${ARLO_BACKUP_DIR_PATH}/${date_formatted}..."
+
+echo "Creating upload directory for transfer..."
 ssh ${ARLO_BACKUP_USER}@${ARLO_BACKUP_IP} mkdir -p ${ARLO_BACKUP_DIR_PATH}/${date_formatted} || exit 1
+echo "Transfering video files over..."
 rsync --remove-source-files -av videos/ ${ARLO_BACKUP_USER}@${ARLO_BACKUP_IP}:${ARLO_BACKUP_UPLOAD_PATH}/${date_formatted} || exit 1
 
 echo "Backup completed"
